9b5b742dc937e9fc4563f877571de605e5f7654e
Rabii <rabi3jouira@gmail.com>
2021-12-14 18:41:40 +0100

    Update TALN.py

--- TALN.py
+++ TALN.py
@@ -29,3 +29,43 @@ for sent in sentences:
     i=i+1
     
 tokenSentences
+
+# ********************************
+
+# split to words
+wordsSPACY = [word.text for word in doc]
+print(wordsSPACY)
+
+# *****************************
+stopwords_spacy = list(STOP_WORDS)
+punctuation
+
+# ****************************
+# Extract the lemma for each word
+finalCleanText = " ".join([token.lemma_ for token in doc])
+finalCleanText
+
+# *************************
+# convert the new lemma text from text type to nlp type
+# to facilate the work later
+doc = nlp(finalCleanText)
+
+# ****************************
+
+# calculat the frequencies of words without considerate the puncts and the stopwords
+word_frequencies = {}
+for word in doc:
+    if word.text.lower() not in stopwords_spacy:
+        if word.text.lower() not in punctuation:
+            if word.text not in word_frequencies.keys():
+                word_frequencies[word.text]=1
+            else:
+                word_frequencies[word.text]+=1
+
+word_frequencies
+
+# **************************************
+# the most word exist
+max_frequency = max(word_frequencies.values())
+max_key = max(word_frequencies,key=word_frequencies.get)
+print (max_key,' : ',max_frequency)
